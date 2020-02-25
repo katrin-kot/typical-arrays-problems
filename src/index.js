677109dd98b8f182709cabad32f927d9a62f52b3
katrin-kot <enkor89@mail.ru>
2020-02-25 11:59:49 +0300

    feat: "all tests are passing"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0 || !Array.isArray(array)) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0 || !Array.isArray(array)) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length === 0 || !Array.isArray(array)) {
+        return 0;
+    }
+        let sum = array.reduce((previous, current) => current += previous);
+        return avg = sum / array.length;
+    
+};
